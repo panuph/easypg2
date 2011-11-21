f51ae8e848482020655f175129c111bceccde922
Panu P <panu@taguchimail.com>
2011-11-21 12:30:18 +1100

    Correct the URL.

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     description="Just another easy way to work with psycopg2.""",
     author="Panu P",
     author_email="panuph@gmail.com",
-    url="https://panuph@github.com/panuph/easypg2.git",
+    url="https://github.com/panuph/easypg2",
     py_modules=["easypg2"]
 )
